c5353a9d128c026bbedbafec79bbb85548455a48
CY <iamCristYE@Gmail.com>
2024-10-18 14:56:01 +0800

    Test

--- nogizaka.py
+++ nogizaka.py
@@ -153,7 +153,7 @@ def get_profile(member_id: int):
     return result
 
 
-def get_articles_url_list(member_id: int):
+def get_articles_url_list(member_id: int, previous_blog_url_list: list):
     current_page = 1
     current_url = (
         f"https://www.nogizaka46.com/s/n46/diary/MEMBER/list?page=0&ct={member_id}"
@@ -172,6 +172,9 @@ def get_articles_url_list(member_id: int):
             url_without_params = urllib.parse.urlunparse(
                 (parsed_url.scheme, parsed_url.netloc, parsed_url.path, "", "", "")
             )
+            # Previously crawled
+            if url_without_params in previous_blog_url_list:
+                return articles_url_list
             articles_url_list.append(url_without_params)
         print(f"{len(a_list)} results on page {current_page}.")
 
@@ -235,18 +238,43 @@ def get_blog_content(url: str, repo_name: str):
             pass
 
 
-def main(member_id: int):
+def update_repo(member_id: int):
     import sys
 
     sys.setrecursionlimit(4646)
     # fix https://www.nogizaka46.com/s/n46/diary/detail/56176
 
-    result = {}
-    profile = get_profile(member_id)
-    for key in profile:
-        result[key] = profile[key]
+    result = get_profile(member_id)
 
-    articles_url_list = get_articles_url_list(member_id)
+    from github import Github
+    import subprocess
+
+    # Replace with your GitHub token and organization name
+    token = os.getenv("Token_GitHub")
+    organization_name = "Sakamichi"
+    repo_name = result["repo_name"]
+    g = Github(token)
+    org = g.get_organization(organization_name)
+    try:
+        repo = org.get_repo(repo_name)
+        print(f"Repository '{repo_name}' already exists.")
+    except:
+        # Create a new repository
+        repo = org.create_repo(name=repo_name)
+        print(f"Creating repository '{repo_name}'.")
+    # clone_url = repo.clone_url
+    clone_url = f"https://{token}@github.com/SakamichiSeries/{repo_name}.git"
+    subprocess.run(["git", "clone", clone_url])
+
+    previous_blog_url_list = []
+    if os.path.exists(repo_name + "/result.json"):
+        with open(repo_name + "/result.json") as previous_json:
+            previous_result = json.load(previous_json)
+            for blog_entry in previous_result["blog"]:
+                previous_blog_url_list.append(blog_entry["url"])
+                print("Previous blog url: " + blog_entry["url"])
+
+    articles_url_list = get_articles_url_list(member_id, previous_blog_url_list)
     result["blog"] = []
 
     for i in range(len(articles_url_list)):
@@ -254,12 +282,167 @@ def main(member_id: int):
         article = get_blog_content(articles_url_list[i], result["repo_name"])
         result["blog"].append(article)
         # time.sleep(random.randint(1, 3))
+
+    # Add back previous results:
+    if os.path.exists(repo_name + "/result.json"):
+        with open(repo_name + "/result.json") as previous_json:
+            for blog_entry in previous_result["blog"]:
+                result["blog"].append(blog_entry)
+
     with open(f"{result['repo_name']}/result.json", "w", encoding="utf-8") as f:
         json.dump(result, f, ensure_ascii=False, indent=2)
 
+    subprocess.run(["pwd"])
+    subprocess.run(["cd", repo_name])
+    subprocess.run(["pwd"])
+    subprocess.run(["cp", "../.nojekyll", "."])
+    subprocess.run(
+        ["git", "config", "--local", "user.name", "GitHub Action"], check=True
+    )
+    subprocess.run(
+        ["git", "config", "--local", "user.email", "action@github.com"], check=True
+    )
 
-from dotenv import load_dotenv
+    # Add all changes
+    subprocess.run(["git", "add", "-A"], check=True)
 
-load_dotenv()
-member_id = os.getenv("MEMBER_ID")
-main(member_id)
+    # Commit the changes
+    subprocess.run(
+        ["git", "commit", "-m", "Automated commit by GitHub Action"], check=True
+    )
+
+    # Push the changes
+    subprocess.run(["git", "push"], check=True)
+
+
+member_code = [
+    36749,  # 伊藤 理々杏
+    36750,  # 岩本 蓮加
+    36751,  # 梅澤 美波
+    36753,  # 久保 史緒里
+    36755,  # 佐藤 楓
+    36756,  # 中村 麗乃
+    36757,  # 向井 葉月
+    36759,  # 吉田 綾乃クリスティー
+    36760,  # 与田 祐希
+    48006,  # 遠藤 さくら
+    48008,  # 賀喜 遥香
+    48010,  # 金川 紗耶
+    48013,  # 柴田 柚菜
+    48015,  # 田村 真佑
+    48017,  # 筒井 あやめ
+    48019,  # 矢久保 美緒
+    55383,  # 黒見 明香
+    55384,  # 佐藤 璃果
+    55385,  # 林 瑠奈
+    55386,  # 松尾 美佑
+    55387,  # 弓木 奈於
+    55388,  # 岩瀬 佑美子
+    55389,  # 井上 和
+    55390,  # 一ノ瀬 美空
+    55391,  # 菅原 咲月
+    55392,  # 小川 彩
+    55393,  # 冨里 奈央
+    55394,  # 奥田 いろは
+    55395,  # 中西 アルノ
+    55396,  # 五百城 茉央
+    55397,  # 池田 瑛紗
+    55400,  # 川﨑 桜
+    55401,  # 岡本 姫奈
+]
+
+for code in member_code:
+    update_repo(code)
+
+# with open("Nogizaka46-member.json") as codes_json:
+#     codes = json.load(codes_json)
+#     for entry in codes["data"]:
+#         print(f"{entry['code']} # {entry['name']}")
+
+# 10001 # 乃木坂46
+# 55401 # 岡本 姫奈
+# 55400 # 川﨑 桜
+# 55397 # 池田 瑛紗
+# 55396 # 五百城 茉央
+# 55395 # 中西 アルノ
+# 55394 # 奥田 いろは
+# 55393 # 冨里 奈央
+# 55392 # 小川 彩
+# 55391 # 菅原 咲月
+# 55389 # 井上 和
+# 55387 # 弓木 奈於
+# 55386 # 松尾 美佑
+# 55385 # 林 瑠奈
+# 55384 # 佐藤 璃果
+# 55383 # 黒見 明香
+# 48014 # 清宮 レイ
+# 48012 # 北川 悠理
+# 48010 # 金川 紗耶
+# 48019 # 矢久保 美緒
+# 48018 # 早川 聖来
+# 48009 # 掛橋 沙耶香
+# 48008 # 賀喜 遥香
+# 48017 # 筒井 あやめ
+# 48015 # 田村 真佑
+# 48013 # 柴田 柚菜
+# 48006 # 遠藤 さくら
+# 36760 # 与田 祐希
+# 36759 # 吉田 綾乃クリスティー
+# 36758 # 山下 美月
+# 36757 # 向井 葉月
+# 36756 # 中村 麗乃
+# 36755 # 佐藤 楓
+# 36754 # 阪口 珠美
+# 36753 # 久保 史緒里
+# 36752 # 大園 桃子
+# 36751 # 梅澤 美波
+# 36750 # 岩本 蓮加
+# 36749 # 伊藤 理々杏
+# 19634 # 相楽 伊織
+# 17822 # 松井 玲奈
+# 17383 # 渡辺 みり愛
+# 17382 # 米徳 京花
+# 17381 # 山崎 怜奈
+# 17380 # 矢田 里沙子
+# 17379 # 寺田 蘭世
+# 17378 # 鈴木 絢音
+# 17377 # 佐々木 琴子
+# 17376 # 伊藤 純奈
+# 17375 # 伊藤 かりん
+# 17068 # 新内 眞衣
+# 16454 # 北野 日奈子
+# 14470 # 堀 未央奈
+# 7639 # 秋元 真夏
+# 284 # 和田 まあや
+# 283 # 若月 佑美
+# 282 # 大和 里菜
+# 281 # 宮澤 成良
+# 280 # 松村 沙友理
+# 279 # 星野 みなみ
+# 278 # 深川 麻衣
+# 277 # 樋口 日奈
+# 276 # 畠中 清羅
+# 275 # 橋本 奈々未
+# 274 # 能條 愛未
+# 273 # 西野 七瀬
+# 272 # 永島 聖羅
+# 271 # 中元 日芽香
+# 270 # 中田 花奈
+# 269 # 高山 一実
+# 268 # 白石 麻衣
+# 267 # 桜井 玲香
+# 266 # 斉藤 優里
+# 265 # 斎藤 ちはる
+# 264 # 齋藤 飛鳥
+# 263 # 川村 真洋
+# 262 # 川後 陽菜
+# 261 # 柏 幸奈
+# 260 # 衛藤 美彩
+# 258 # 井上 小百合
+# 257 # 伊藤 万理華
+# 256 # 伊藤 寧々
+# 255 # 市來 玲奈
+# 55388 # 岩瀬 佑美子
+# 254 # 生駒 里奈
+# 253 # 生田 絵梨花
+# 55390 # 一ノ瀬 美空
